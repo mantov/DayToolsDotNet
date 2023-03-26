06235d3a40d54df84f18d238d0efd865eca72232
mantov <mantovanixande@yahoo.com.br>
2023-03-26 16:23:31 -0300

    Implementação do ipsum lorem

---
+++ DayToolsDotNet/DayToolsDotNet/Commands/CopiaEColaArgs.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+
+namespace DayToolsDotNet.Commands
+{
+    internal class CopiaEColaArgs : CommandArgs
+    {
+        public CopiaEColaArgs(ITextView textView, IVsTextBuffer buffer)
+            
+        {
+        }
+    }
+}
---
+++ DayToolsDotNet/DayToolsDotNet/Commands/IpsumLoremCommand.cs
@@ -0,0 +1,23 @@
+﻿using DayToolsDotNet;
+using Microsoft.VisualStudio.Text;
+using System;
+using Community.VisualStudio.Toolkit;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+
+namespace DayToolsDotNet.Commands
+{
+    [Command(PackageIds.IpsumLoremCommand)]
+    internal sealed class IpsumLoremCommand : BaseCommand<IpsumLoremCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await Package.JoinableTaskFactory.SwitchToMainThreadAsync();
+            DocumentView docView = await VS.Documents.GetActiveDocumentViewAsync();
+            if (docView?.TextView == null) return;
+            SnapshotPoint position = docView.TextView.Caret.Position.BufferPosition;
+            var textoIpsum = new NLipsum.Core.LipsumGenerator().GenerateLipsum(1000);
+            docView.TextBuffer?.Insert(position, textoIpsum);
+        }
+    }
+}
